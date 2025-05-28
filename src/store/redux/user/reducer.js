22799471864e6bc3b8b6a1f07efd659979fa42ab
Trịnh Đồng <trinhdong255@gmail.com>
2025-05-28 15:15:31 +0700

    feat: change architecture to store data

--- src/store/redux/user/reducer.js
+++ src/store/redux/user/reducer.js
@@ -1,108 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { authApi } from "@/services/api/auth";
 
 const userSlice = createSlice({
   name: "user",
   initialState: {
-    user: null,
+    id: null,
+    name: null,
+    email: null,
+    avatarUrl: null,
+    dob: null,
+    gender: null,
   },
   reducers: {
     setUser: (state, action) => {
-      state.user = action.payload;
+      state.id = action.payload.id;
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+      state.avatarUrl = action.payload.avatarUrl;
+      state.dob = action.payload.dob;
+      state.gender = action.payload.gender;
     },
     updateUser: (state, action) => {
-      state.user = action.payload;
+      state.id = action.payload.id || state.id;
+      state.name = action.payload.name || state.name;
+      state.email = action.payload.email || state.email;
+      state.avatarUrl = action.payload.avatarUrl || state.avatarUrl;
+      state.dob = action.payload.dob || state.dob;
+      state.gender = action.payload.gender || state.gender;
     },
     clearUser: (state) => {
-      state.user = null;
+      state.id = null;
+      state.name = null;
+      state.email = null;
+      state.avatarUrl = null;
+      state.dob = null;
+      state.gender = null;
     },
-  },
-  extraReducers: (builder) => {
-    // Đồng bộ khi đăng nhập thành công
-    builder.addMatcher(
-      authApi.endpoints.login.matchFulfilled,
-      (state, action) => {
-        state.user = {
-          code: action.payload.code,
-          message: action.payload.message,
-          result: {
-            accessToken: action.payload.result.accessToken,
-            refreshToken: action.payload.result.refreshToken,
-            authenticated: action.payload.result.authenticated,
-            email: action.payload.result.email,
-          },
-        };
-      }
-    );
-
-    // Đồng bộ khi lấy thông tin người dùng hiện tại
-    builder.addMatcher(
-      authApi.endpoints.getMyInfo.matchFulfilled,
-      (state, action) => {
-        if (state.user) {
-          state.user = {
-            ...state.user,
-            code: action.payload.code,
-            message: action.payload.message,
-            result: {
-              ...state.user.result,
-              id: action.payload.result?.id,
-              name: action.payload.result?.name,
-              email: action.payload.result?.email,
-              roles: action.payload.result?.roles,
-            },
-          };
-        } else {
-          state.user = {
-            code: action.payload.code,
-            message: action.payload.message,
-            result: {
-              id: action.payload.result?.id,
-              name: action.payload.result?.name,
-              email: action.payload.result?.email,
-              roles: action.payload.result?.roles,
-            },
-          };
-        }
-      }
-    );
-
-    // Đồng bộ khi cập nhật thông tin người dùng
-    builder.addMatcher(
-      authApi.endpoints.updateUser.matchFulfilled,
-      (state, action) => {
-        if (state.user) {
-          state.user = {
-            ...state.user,
-            code: action.payload.code,
-            message: action.payload.message,
-            result: {
-              ...state.user.result,
-              id: action.payload.result?.id,
-              name: action.payload.result?.name,
-              email: action.payload.result?.email,
-              roles: action.payload.result?.roles,
-            },
-          };
-        } else {
-          state.user = {
-            code: action.payload.code,
-            message: action.payload.message,
-            result: {
-              id: action.payload.result?.id,
-              name: action.payload.result?.name,
-              email: action.payload.result?.email,
-              roles: action.payload.result?.roles,
-            },
-          };
-        }
-      }
-    );
   },
 });
 
 export const { setUser, updateUser, clearUser } = userSlice.actions;
-export const selectUser = (state) => state.user.user;
+export const selectUser = (state) => state.user;
 export const selectUserId = (state) => state.user.user?.id;
 
 export default userSlice.reducer;
