b084da7b98cc3ae28661542a7c2986346627127b
trinhdong2505 <trinhdong2505@gmail.com>
2025-07-12 16:09:01 +0700

    feat(api): add component FormControl and Select to handle fetch data district by wards match id of district

--- src/pages/Admin/shared/WardsManagement/index.jsx
+++ src/pages/Admin/shared/WardsManagement/index.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
+import {
+  Typography,
+  Box,
+  Button,
+  Snackbar,
+  Alert,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { Typography, Box, Button, Snackbar, Alert } from "@mui/material";
-import DashboardLayoutWrapper from "@/layouts/DashboardLayout";
-
-import { useListWardsQuery } from "@/services/api/ward";
 import { Refresh } from "@mui/icons-material";
+import DashboardLayoutWrapper from "@/layouts/DashboardLayout";
+import { useListWardsQuery } from "@/services/api/ward";
+import {
+  useListDistrictsQuery,
+  useListWardsByDistrictQuery,
+} from "@/services/api/district";
 import ErrorDisplay from "@/components/ErrorDisplay";
+import { skipToken } from "@reduxjs/toolkit/query";
 
 const WardsManagement = () => {
+  const [selectedDistrictId, setSelectedDistrictId] = useState("");
   const [paginationModel, setPaginationModel] = useState({
     page: 0,
     pageSize: 20,
@@ -22,7 +37,7 @@ const WardsManagement = () => {
     data: dataWards,
     isLoading: isLoadingWards,
     isError: isErrorWards,
-    refetch,
+    refetch: refetchDataWards,
   } = useListWardsQuery(
     {
       page: paginationModel.page,
@@ -33,9 +48,49 @@ const WardsManagement = () => {
     }
   );
 
+  const { data: dataDistricts, isError: isErrorDistricts } =
+    useListDistrictsQuery(
+      { page: 0, size: 1000 },
+      {
+        refetchOnMountOrArgChange: true,
+      }
+    );
+
+  const {
+    data: dataWardsByDistrict,
+    isLoading: isLoadingWardsByDistrict,
+    isError: isErrorWardsByDistrict,
+    refetch: refetchDataWardsByDistrict,
+  } = useListWardsByDistrictQuery(
+    selectedDistrictId
+      ? {
+          id: selectedDistrictId,
+          page: paginationModel.page,
+          size: paginationModel.pageSize,
+        }
+      : skipToken,
+    {
+      refetchOnMountOrArgChange: true,
+    }
+  );
+
+  const isLoading = selectedDistrictId
+    ? isLoadingWardsByDistrict
+    : isLoadingWards;
+
   const dataRowWards =
-    dataWards?.result?.items?.map((item) => ({ ...item, id: item.code })) || [];
-  const totalRows = dataWards?.result?.totalItems || 0;
+    (selectedDistrictId
+      ? dataWardsByDistrict?.result?.items
+      : dataWards?.result?.items) || [];
+
+  const rows = dataRowWards.map((item) => ({
+    ...item,
+    id: item.code,
+  }));
+
+  const totalRows = selectedDistrictId
+    ? dataWardsByDistrict?.result?.totalItems
+    : dataWards?.result?.totalItems || 0;
 
   const columnsWards = [
     { field: "code", headerName: "Code", width: 150 },
@@ -47,20 +102,24 @@ const WardsManagement = () => {
   };
 
   const handleRefresh = () => {
-    refetch();
+    if (selectedDistrictId) {
+      refetchDataWardsByDistrict();
+    } else {
+      refetchDataWards();
+    }
     setSnackbar({
       open: true,
-      message: "Danh sách phường / xã đã được làm mới !",
+      message: "Danh sách phường / xã đã được làm mới!",
       severity: "info",
     });
   };
 
-  if (isErrorWards) {
+  if (isErrorWards || isErrorWardsByDistrict || isErrorDistricts) {
     return (
       <ErrorDisplay
         error={{
           message:
-            "Không tải được danh sách phường / xã. Vui lòng kiểm tra kết nối của bạn và thử lại !",
+            "Không tải được dữ liệu. Vui lòng kiểm tra kết nối của bạn và thử lại!",
         }}
       />
     );
@@ -72,16 +131,43 @@ const WardsManagement = () => {
         Quản lý phường / xã
       </Typography>
 
-      <Button
-        variant="outlined"
-        onClick={handleRefresh}
-        startIcon={<Refresh />}
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
         sx={{ mb: 2 }}
       >
-        Làm mới
-      </Button>
+        <Button
+          variant="outlined"
+          onClick={handleRefresh}
+          startIcon={<Refresh />}
+        >
+          Làm mới
+        </Button>
 
-      <Box height={500} width={"100%"}>
+        <FormControl sx={{ minWidth: 300 }}>
+          <InputLabel>Chọn quận / huyện</InputLabel>
+          <Select
+            value={selectedDistrictId}
+            label="Chọn quận / huyện"
+            onChange={(e) => {
+              setPaginationModel({
+                page: 0,
+                pageSize: paginationModel.pageSize,
+              });
+              setSelectedDistrictId(e.target.value);
+            }}
+          >
+            {dataDistricts?.result?.items?.map((district) => (
+              <MenuItem key={district.id} value={district.id}>
+                {district.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+
+      <Box height={500} width="100%">
         <DataGrid
           sx={{
             boxShadow: 2,
@@ -92,8 +178,8 @@ const WardsManagement = () => {
             },
           }}
           columns={columnsWards}
-          rows={dataRowWards}
-          loading={isLoadingWards}
+          rows={rows}
+          loading={isLoading}
           disableSelectionOnClick
           slotProps={{
             loadingOverlay: {
@@ -101,9 +187,7 @@ const WardsManagement = () => {
               noRowsVariant: "linear-progress",
             },
           }}
-          localeText={{
-            noRowsLabel: "Không có dữ liệu",
-          }}
+          localeText={{ noRowsLabel: "Không có dữ liệu" }}
           pagination
           paginationMode="server"
           sortingMode="server"
