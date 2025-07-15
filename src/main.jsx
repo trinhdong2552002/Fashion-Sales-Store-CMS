fcc866e70aadc3136a334c09f7fafe9e4898ee98
Trịnh Đồng <trinhdong255@gmail.com>
2025-07-15 20:52:45 +0700

    chore(styles): remove index.css in favor of MUI CssBaseline

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
-import "./index.css";
 import { PersistGate } from "redux-persist/integration/react";
-
 import App from "./App.jsx";
-
 import { Provider } from "react-redux";
-
 import { persistor, store } from "@/store";
 
 createRoot(document.getElementById("root")).render(
